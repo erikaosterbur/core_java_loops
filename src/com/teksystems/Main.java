2489ae3419f9ac56019e06ace048f9fe23a3cf99
erikaosterbur <erikaosterbur@gmail.com>
2022-02-23 20:14:33 -0600

    Problem four finished

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -5,10 +5,12 @@ public class Main {
     public static void main(String[] args) {
         problemOne();
         problemTwo();
+        problemThree();
+        problemFour();
     }
     public static void problemOne () {
         /*Write a program that uses a for-loop to loop through the numbers 1-10 and prints out each number.*/
-        for(int i = 1; i < 11; i++){
+        for(int i = 1; i <= 10; i++){
             System.out.println(i);
         }
     }
@@ -19,6 +21,28 @@ public class Main {
         while (count < 101) {
             System.out.println(count);
             count+=10;
+        }
+    }
+
+    public static void problemThree () {
+    /*Write a program that uses a do-while-loop to loop through the numbers 1-10 and prints out each number.*/
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        } while (i<=10);
+    }
+
+    public static void problemFour () {
+        /*Write a program that uses a for-loop to loop through the numbers 1-100.
+        Print out each number if is a multiple of 5, but do not print out any numbers between 25 and 75.
+        Use the “continue” statement to accomplish this. */
+        for(int i = 1; i <= 100; i++){
+            if(i > 25 && i < 75){
+                continue;
+            } else if(i % 5 == 0){
+                System.out.println(i);
+            }
         }
     }
 }
