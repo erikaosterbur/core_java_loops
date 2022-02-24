e30d9fb627daf5bc3f056b7348633591f046dc4b
erikaosterbur <erikaosterbur@gmail.com>
2022-02-23 19:48:56 -0600

    Initial commit

--- src/com/teksystems/Main.java
+++ src/com/teksystems/Main.java
@@ -3,6 +3,22 @@ package com.teksystems;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        problemOne();
+        problemTwo();
+    }
+    public static void problemOne () {
+        /*Write a program that uses a for-loop to loop through the numbers 1-10 and prints out each number.*/
+        for(int i = 1; i < 11; i++){
+            System.out.println(i);
+        }
+    }
+
+    public static void problemTwo () {
+        /*Write a program that uses a while loop to loop through the numbers 0-100 in increments of 10 and prints out each number.*/
+        int count = 0;
+        while (count < 101) {
+            System.out.println(count);
+            count+=10;
+        }
     }
 }
